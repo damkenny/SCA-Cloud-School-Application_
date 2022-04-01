afe5b8e7e917cf0b8b24311fcc7d67fd512ce14a
ADENIYE KEHINDE <adeniyekehinde0@gmail.com>
2022-04-01 07:43:28 +0000

    puhing to the start branch

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Welcome to SCA Cloud School Application</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
